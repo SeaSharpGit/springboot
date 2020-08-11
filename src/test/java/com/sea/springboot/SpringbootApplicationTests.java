495f3cdfce933c9b2168c0a8dabe277495f833ee
Sea for Mac <18912387311@163.com>
2020-08-12 01:05:19 +0800

    日志

--- src/test/java/com/sea/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/sea/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,18 @@
 package com.sea.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringbootApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("1");
+        log.info("2");
+        log.info("3");
     }
 
 }
