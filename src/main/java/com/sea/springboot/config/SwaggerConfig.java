fb4b290daeea97ebbaba3beab4022131ccbf75d2
Sea for Mac <18912387311@163.com>
2020-08-13 23:56:57 +0800

    swagger2

---
+++ src/main/java/com/sea/springboot/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.sea.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sea.springboot.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger2")
+                        .description("API 描述")
+                        .version("1.0")
+                        .contact(new Contact("大海","https://github.com/seasharpgit","18912387311@163.com"))
+                        .license("The Apache License")
+                        .licenseUrl("https://github.com/seasharpgit")
+                        .build());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("SpringBoot整合Swagger2")
+                //创建人
+                .contact(new Contact("大海", "https://github.com/seasharpgit/", "18912387311@163.com"))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
+
+
+}
--- src/main/java/com/sea/springboot/controller/HomeController.java
+++ src/main/java/com/sea/springboot/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.sea.springboot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping
+@RequestMapping("home")
+@Api(tags = "用户操作接口")
 public class HomeController {
 
     @GetMapping("test")
+    @ApiOperation("添加用户的接口")
     public String test(){
         return "hello world!";
     }
