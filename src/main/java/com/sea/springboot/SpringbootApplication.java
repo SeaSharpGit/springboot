3fbfdcc345efe13fac497f7adb814001969cf31a
王海 <18912387311@163.com>
2020-09-08 14:31:31 +0800

    集成MyBaits

--- src/main/java/com/sea/springboot/SpringbootApplication.java
+++ src/main/java/com/sea/springboot/SpringbootApplication.java
@@ -1,11 +1,13 @@
 package com.sea.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableOpenApi
+@MapperScan(basePackages = "com.sea.springboot.master.mapper")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
