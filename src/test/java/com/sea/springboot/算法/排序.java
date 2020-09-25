e440e0925bbdac407fbd67a4b3240a015f026541
王海 <18912387311@163.com>
2020-09-25 15:45:04 +0800

    归并排序

--- src/test/java/com/sea/springboot/算法/排序.java
+++ src/test/java/com/sea/springboot/算法/排序.java
@@ -8,13 +8,19 @@ public class 排序 {
 
     @Test
     void test() {
-        int[] arrs = new int[]{7, 2, 4, 2, 8, 1, 9, 8, 21, 27};
-        插入排序(arrs);
+        int[] arrs = new int[]{7, 2, 4, 2, 8, 1, 9, 8, 27, 21};
+        order4(arrs);
         System.out.println(arrs);
         int a = 1;
     }
 
-    private void 冒泡排序(int[] arrs) {
+    /**
+     * 冒泡排序
+     * 优化：当顺序没有发生改变时，后面的就不需要执行了
+     *
+     * @param arrs
+     */
+    private void order(int[] arrs) {
         if (arrs.length <= 1) {
             return;
         }
@@ -34,7 +40,12 @@ public class 排序 {
         }
     }
 
-    private void 插入排序(int[] arrs) {
+    /**
+     * 插入排序
+     *
+     * @param arrs
+     */
+    private void order2(int[] arrs) {
         if (arrs.length <= 1) {
             return;
         }
@@ -49,6 +60,103 @@ public class 排序 {
                 }
             }
             arrs[j + 1] = temp;
+        }
+    }
+
+    /**
+     * 选择排序
+     * 优化：如果当前位置就是最小值，就不需要交换
+     * 最后一个元素不需要判断，自然就是最大值，所以循环次数-1
+     *
+     * @param arrs
+     */
+    private void order3(int[] arrs) {
+        if (arrs.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < arrs.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arrs.length; j++) {
+                if (arrs[j] < arrs[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = arrs[i];
+                arrs[i] = arrs[min];
+                arrs[min] = temp;
+            }
+        }
+    }
+
+    /**
+     * 归并排序
+     * 不是原地排序，每次排序都要申请额外的空间
+     *
+     * @param arrs
+     */
+    private void order4(int[] arrs) {
+        if (arrs.length <= 1) {
+            return;
+        }
+        MergeSort(arrs, 0, arrs.length - 1);
+    }
+
+    private void MergeSort(int[] arrs, int m, int n) {
+        if (m >= n) {
+            return;
+        }
+        int x = (m + n) / 2;
+        MergeSort(arrs, m, x);
+        MergeSort(arrs, x + 1, n);
+        Merge(arrs, m, x, n);
+    }
+
+    private void Merge(int[] arrs, int m, int x, int n) {
+        int i = m;
+        int j = x + 1;
+        int k = 0;
+        int[] temp = new int[n - m + 1];
+        while (i <= x && k <= n) {
+            if (arrs[i] <= arrs[j]) {
+                temp[k] = arrs[i];
+                i++;
+            } else {
+                temp[k] = arrs[j];
+                j++;
+            }
+            k++;
+        }
+        // 把左边剩余的数移入数组
+        while (i <= x) {
+            temp[k++] = arrs[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while (j <= n) {
+            temp[k++] = arrs[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        for (i = 0; i <= n - m; i++) {
+            arrs[m + i] = temp[i];
+        }
+    }
+
+    private void 快速排序(int[] arrs) {
+        if (arrs.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < arrs.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arrs.length; j++) {
+                if (arrs[j] < arrs[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = arrs[i];
+                arrs[i] = arrs[min];
+                arrs[min] = temp;
+            }
         }
     }
 
